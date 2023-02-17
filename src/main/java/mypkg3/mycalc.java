03f64e48ee08b527af4037346d4561202ee2db85
shambhavee <shambhaveesrivastava@gmail.com>
2023-02-17 22:20:29 +0530

    first commit

---
+++ src/main/java/mypkg3/mycalc.java
@@ -0,0 +1,20 @@
+package mypkg3;
+
+public class mycalc {
+	public void display() {
+		System.out.println("dev, welcome to jenkins");
+	}
+	public void myname()
+	{
+		System.out.println("hello shambhavee");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		mycalc obj = new mycalc();
+		obj.display();
+		obj.myname();
+
+	}
+
+}
