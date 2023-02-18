30861f7879219f7685e36ec4b51e6433d4f179b9
shambhavee <shambhaveesrivastava@gmail.com>
2023-02-18 21:46:59 +0530

    first commit

---
+++ src/main/java/mypkgg1/mydisplay.java
@@ -0,0 +1,15 @@
+package mypkgg1;
+
+public class mydisplay {
+	public void myshow() {
+		System.out.println("hello devops! Welcome to jenkins");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mydisplay obj = new mydisplay();
+		obj.myshow();
+
+	}
+
+}
